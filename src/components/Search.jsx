d5e2193e03267b6254bfdfc4b33826016c8a022a
jmzqq <joaomwguel@gmail.com>
2024-09-30 16:59:42 -0300

    erro updates

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,23 @@
 import axios from "axios";
 import { useState } from "react";
 import EarthFox from "../assets/earthfox_logo.png";
+import "../App.css";
 
 const Search = () => {
   const [query, setQuery] = useState("");
   const [results, setResults] = useState([]);
   const [error, setError] = useState("");
-  
+  const [loading, setLoading] = useState(false);
+
   const URL = "https;//serpapi.com/search.json";
   const API_KEY =
     "3d6630aab4a10b256e438863fe496beb3d9d140fe74e0f3aabbfed36bc924bda";
-  
+
   const handleSubmit = async (event) => {
     event.preventDefault();
+    if (  query) {
+      return;
+    }
 
     try {
       const res = await axios.get(URL, {
@@ -24,16 +29,18 @@ const Search = () => {
           h1: "pt-br",
           gl: "br",
           num: 10,
-        }
-      })
+        },
+      });
       const data = await res.json();
       setResults(data);
-    }catch (err) {
+    } catch (err) {
       console.error(err);
-      setError("Aconteceu um erro ao tentar fazer a busca.")
+      setError("Aconteceu um erro ao tentar fazer a busca.");
+    } finally {
+      setLoading(false);
     }
   };
-  
+
   return (
     <div className="App">
       <div className="Logo">
@@ -45,10 +52,30 @@ const Search = () => {
           <input
             className="Search-input"
             type="text"
-            placeholder="O que você está procurando?"
+            placeholder="Pesquise com Google ou digite um endereço"
             onChange={(event) => setQuery(event.target.value)}
           />
         </form>
+        <div>
+          <ul>
+            {error ? (
+              <h4>{error}</h4>
+            ) : loading ? (
+              <p>Carregando...</p>
+            ) : (
+              results.map((r, index) => {
+                return (
+                  <li key={index.id}>
+                    <a href={r.link} target="_blank" rel="noopener noreferrer">
+                      {r.title}
+                    </a>
+                    <p>{r.snippet}</p>
+                  </li>
+                );
+              })
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
